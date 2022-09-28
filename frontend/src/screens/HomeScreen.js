90a8d7a7dd6b514a25871879abc108ed570d63e9
Aghilashub2020 <aghilasrabia15@gmail.com>
2022-09-27 21:12:53 -0400

    booststrap setup

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Row, Col } from "react-bootstrap";
+import product from "../components/Product";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeScreen
